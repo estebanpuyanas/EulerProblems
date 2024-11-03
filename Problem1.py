25c98534e65bb3398b1db60962e8d4335932f603
Esteban Puyana <epuyana@sqg.co>
2024-11-03 16:23:47 -0500

    Euler Problems 1,2 + README

---
+++ Problem1.py
@@ -0,0 +1,24 @@
+"""
+Problem 1: Multiples of 3 or 5
+Objective: Find the sum of all the multiples below 3 or 5 below 1000
+"""
+
+def multipleCounter(limit):
+
+    result_list = []
+
+    for i in range(limit):
+        if i % 3 == 0 or i % 5 == 0:
+            result_list.append(i)
+    
+    sum_result = sum(result_list)
+
+    return sum_result
+
+
+def main():
+
+    result = multipleCounter(1000)
+    print(result)
+
+main()
